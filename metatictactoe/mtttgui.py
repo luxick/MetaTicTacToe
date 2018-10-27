830f631daafe6bf1b822867e8043ada3c021d654
luxick <git@luxick.de>
2018-10-27 15:30:10 +0200

    Detect clicks in game grid.

--- metatictactoe/mtttgui.py
+++ metatictactoe/mtttgui.py
@@ -5,6 +5,8 @@ from mttt import MetaTicTacToe
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 700
 
+MARGIN = 5
+
 
 class GameUI(arcade.Window):
     mttt_board = None
@@ -19,8 +21,12 @@ class GameUI(arcade.Window):
 
         arcade.set_background_color(arcade.color.WHITE)
 
-        self.clock_x = 3
-        self.clock_y = self.height - 20
+        self.game_area_size = 600
+        self.game_area_x = self.width - self.game_area_size - MARGIN
+        self.game_area_y = self.height - self.game_area_size - MARGIN
+
+        self.clock_width = MARGIN
+        self.clock_height = self.height - 15 - MARGIN
 
     def setup(self):
         # Create your sprites and sprite lists here
@@ -34,13 +40,55 @@ class GameUI(arcade.Window):
         # the screen to the background color, and erase what we drew last frame.
         arcade.start_render()
 
+        self.draw_clock()
+        self.draw_game_area()
+
+    def draw_game_area(self):
+        # Draw an outline around the game area
+        board_x_center = self.game_area_x + self.game_area_size / 2
+        board_y_center = self.game_area_y + self.game_area_size / 2
+        arcade.draw_rectangle_outline(board_x_center, board_y_center,
+                                      self.game_area_size, self.game_area_size,
+                                      arcade.color.GRAY_BLUE)
+
+        # Draw the meta board
+        self.draw_board(self.game_area_x, self.game_area_y, self.game_area_size, arcade.color.BLACK)
+
+        # Draw the boards
+        board_size = self.game_area_size // 3
+        for x in range(self.game_area_x,
+                       self.game_area_x + self.game_area_size,
+                       self.game_area_size // 3):
+            for y in range(self.game_area_y,
+                           self.game_area_y + self.game_area_size,
+                           self.game_area_size // 3):
+                self.draw_board(x, y, board_size, arcade.color.GRAY_BLUE)
+
+    @staticmethod
+    def draw_board(pos_x, pos_y, size, color):
+        # Draw vertical lines
+        for x in range(pos_x,
+                       pos_x + size,
+                       size // 3):
+            arcade.draw_line(x, pos_y,
+                             x, pos_y + size,
+                             color)
+        # Draw horizontal lines
+        for y in range(pos_y,
+                       pos_y + size,
+                       size // 3):
+            arcade.draw_line(pos_x, y,
+                             pos_x + size, y,
+                             arcade.color.GRAY_BLUE)
+
+    def draw_clock(self):
         minutes, seconds = 0, 0
         if self.game_start:
             diff = int(time.time() - self.game_start)
             minutes, seconds = diff // 60, diff % 60
         arcade.draw_text('Time: {:02}:{:02}'.format(minutes, seconds),
-                         self.clock_x,
-                         self.clock_y,
+                         self.clock_width,
+                         self.clock_height,
                          arcade.color.BLACK)
 
     def update(self, delta_time):
@@ -75,9 +123,48 @@ class GameUI(arcade.Window):
         """
         Called when the user presses a mouse button.
         """
-        print(x, y, button, key_modifiers)
+        if not self.game_area_hit(x, y):
+            return
+
+        br, bc, fr, fc = self.get_grid_coordinates(x, y)
+
         if not self.game_start:
             self.game_start = time.time()
+
+    def game_area_hit(self, x, y):
+        if x < self.game_area_x or x > self.game_area_x + self.game_area_size:
+            return False
+        if y < self.game_area_y or y > self.game_area_y + self.game_area_size:
+            return False
+        return True
+
+    def get_grid_coordinates(self, x, y):
+        # Get board
+        board_size = self.game_area_size // 3
+        column, row = self.get_cell_from_coordinates(x, y,
+                                                     self.game_area_x, self.game_area_y,
+                                                     board_size)
+        # Get Field
+        board_x = self.game_area_x + column * board_size
+        board_y = self.game_area_y + (self.game_area_size - board_size) - row * board_size
+        field_column, field_row = self.get_cell_from_coordinates(x, y,
+                                                                 board_x, board_y,
+                                                                 board_size // 3)
+
+        print(f'Coordinates: (x={x}, y={y})\n'
+              f'Board: (row={row}, column={column})\n'
+              f'Field: (row={field_row}, column={field_column})\n'
+              f'MTTTObject: (game[{row}][{column}][{field_row}][{field_column}])\n')
+
+        return row, column, field_row, field_column
+
+    @staticmethod
+    def get_cell_from_coordinates(x, y, rel_x, rel_y, cell_size):
+        column = (x - rel_x) // cell_size
+        row = (y - rel_y) // cell_size
+        # Flip row coordinate. 0, 0 should be top left
+        row = abs(row - 2)
+        return column, row
 
     def on_mouse_release(self, x, y, button, key_modifiers):
         """
